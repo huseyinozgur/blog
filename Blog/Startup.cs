9705e9dde0c21c5313da8f1e15d07fff0c976db2
Hüseyin Özgür <huseyin.ozgur@vektora.com>
2019-03-22 18:15:19 +0300

    commit

--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -24,6 +24,7 @@ namespace Blog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
